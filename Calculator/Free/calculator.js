728342a013ce35056f6ff097d46cd3423afa6bba
Prince Karki <PrinceKarki121@gmail.com>
2026-06-26 10:03:14 +0545

    wrote logic using function instead of in-built function

--- Calculator/Free/calculator.js
+++ Calculator/Free/calculator.js
@@ -1,18 +1,51 @@
-const display = document.getElementById('display');
+const display = document.getElementById("display");
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+let ans = 0;
 
 
-function appendToDisplay(input){
-    display.value += input;
+
+function appendToDisplay(input) {
+
+    if(input === "+" ||
+    input === "-" ||
+    input === "*" ||
+    input === "/"
+){
+        operator = input;
+    } else if( operator === "") {
+        num1 = num1 + input;
+    }else {
+        num2 = num2 + input;
+    }
+        display.value = num1 + operator + num2;
 }
+
+
+function clearDisplay() {
+    num1 = "";
+    num2 = "";
+    operator = "";
+    display.value = "";
+}
+
 
 function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch {
-        display.value = "Error";
-    }
-}
+   num1 = Number(num1);
+   num2 = Number(num2);
 
-function clearDisplay(){
-      display.value = "";
-} 
+   if(operator === '+'){
+    display.value = num1 + num2;
+   }
+   else if(operator === '-'){
+    display.value = num1 - num2;
+   }
+    else if(operator === '*'){
+    display.value = num1 * num2;
+   }
+    else if(operator === '/'){
+    display.value = num1 / num2;
+   }
+}
